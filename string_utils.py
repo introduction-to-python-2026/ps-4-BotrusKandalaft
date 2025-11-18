463deb0732c956f6f408d8991ddc93ddbadd84b6
BotrusKandalaft <139566585+BotrusKandalaft@users.noreply.github.com>
2025-11-18 16:48:10 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,5 +1,16 @@
 def split_before_each_uppercases(formula):
-    pass # Replace the `pass` with your code
+  split_formula = []
+  start = 0
+  if not formula:
+    return []
+  for end in range(1, len(formula)):
+    if formula[end].isupper():
+      split_formula.append(formula[start:end])
+      start = end
+  split_formula.append(formula[start:])
+  return split_formula
+
+
 
 
 def split_at_first_digit(formula):
